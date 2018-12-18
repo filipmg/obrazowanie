956a6ec5d4ae660863b6d9e8ac61853bb5622087
filipmg <filip.m.guzy@gmail.com>
2018-12-18 21:00:07 +0100

    Thresholding for image values is needed in order to use Jaccard index

--- image_processor.py
+++ image_processor.py
@@ -2,6 +2,7 @@ from RidgeDetection.ridge_detector import RidgeDetector, DetectionType
 import glob
 import cv2
 import os
+import statistics
 
 
 class ImageProcessor:
@@ -39,12 +40,8 @@ class ImageProcessor:
             image = (image[0], cv2.cvtColor(image[1], cv2.COLOR_BGR2GRAY))
             out_ridge_opencv = self.ridgeDetector.detect_ridges(image[1], DetectionType.OPENCV)
             out_ridge_custom = self.ridgeDetector.detect_ridges(image[1], DetectionType.CUSTOM)
-            norm_image_out_ridge_opencv = cv2.normalize(out_ridge_opencv, None, alpha=0, beta=1,
-                                                        norm_type=cv2.NORM_MINMAX, dtype=cv2.CV_32F)
-            norm_image_out_ridge_custom = cv2.normalize(out_ridge_custom, None, alpha=0, beta=1,
-                                                        norm_type=cv2.NORM_MINMAX, dtype=cv2.CV_32F)
-            self.output_data["ridge-opencv"].append((image[0], norm_image_out_ridge_opencv.round()))
-            self.output_data["ridge-custom"].append((image[0], norm_image_out_ridge_custom.round()))
+            self.output_data["ridge-opencv"].append((image[0], out_ridge_opencv))
+            self.output_data["ridge-custom"].append((image[0], out_ridge_custom))
 
     def get_processed_data(self):
         return self.output_data
--- jaccard_calculator.py
+++ jaccard_calculator.py
@@ -1,6 +1,7 @@
 from sklearn.metrics import jaccard_similarity_score
 import glob
 import cv2
+import statistics
 
 
 class JaccardCalculator:
@@ -19,15 +20,14 @@ class JaccardCalculator:
         self.file_list = glob.glob(self.manual_dir)
         for filename in self.file_list:
             image = cv2.cvtColor(cv2.imread(filename), cv2.COLOR_BGR2GRAY)
-            norm_image = cv2.normalize(image, None, alpha=0, beta=1,
-                                       norm_type=cv2.NORM_MINMAX, dtype=cv2.CV_32F)
-            self.manual_images.append(norm_image.round())
+            image[image == 255] = 1
+            self.manual_images.append(image)
 
     def calculate_coefficients(self):
         for dataset_name, images in self.output_data.items():
             self.jaccard_scores[dataset_name] = []
             for manual_image, processed_image in zip(self.manual_images, images):
-                score = jaccard_similarity_score(manual_image.ravel(), processed_image[1].ravel().round())
+                score = jaccard_similarity_score(manual_image.ravel(), processed_image[1].ravel())
                 self.jaccard_scores[dataset_name].append(score)
 
         print(self.jaccard_scores)
