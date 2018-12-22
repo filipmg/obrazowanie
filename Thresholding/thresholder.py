69608d6719c3fb7d50c9d680bf6624881d7f1e6a
Łukasz Michalczak <lukasz.michalczak@capgemini.com>
2018-12-22 18:27:13 +0100

    custom threasholding

--- Thresholding/thresholder.py
+++ Thresholding/thresholder.py
@@ -1,20 +1,20 @@
 from skimage.filters import threshold_local
-from enum import Enum
 import cv2
+from enum import Enum
 
-class AdaptiveThreshType(Enum):
-    GAUSSIAN = 1
-    MEAN = 2
+class DetectionType(Enum):
+    CUSTOM = 1
+    OPENCV = 2
 
 class Thresholder:
-
-    def thresh(self, image, adaptiveThreshType):
-        if adaptiveThreshType == AdaptiveThreshType.MEAN:
+    
+    def thresh(self, image, detection_type, mask=None):
+        if detection_type == DetectionType.CUSTOM:
+            out_image = self.__detect_using_custom(image, mask)
+        elif detection_type == DetectionType.OPENCV:
             out_image = self.__detect_using_mean(image)
-        elif adaptiveThreshType == AdaptiveThreshType.GAUSSIAN:
-            out_image = self.__detect_using_gaussian(image)
         else:
-            out_image = self.__detect_using_gaussian(image)
+            out_image = self.__detect_using_mean(image)
 
         return out_image
 
@@ -26,3 +26,25 @@ class Thresholder:
         binary_local = cv2.adaptiveThreshold(image, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY_INV, 11, 2)
         return binary_local
         
+    def __detect_using_custom(self, colorImage, mask):
+        blueChan, greenChan, redChan = cv2.split(colorImage)
+        
+        histogramGreen = cv2.calcHist([greenChan], [0], mask, [256], [0, 256])
+
+        threshValueGreen = self.calculateThreshValue(histogramGreen)
+
+        self.threshAtValue(greenChan, threshValueGreen)
+
+        return greenChan
+
+    def calculateThreshValue(self, histogram):
+        sum = 0
+        for i in range(0, 256):
+            sum += histogram[i][0]
+            if sum >= 15000:
+                return i
+
+
+    def threshAtValue(self, image, treshValue):
+        image[image >= treshValue] = 255
+        image[image < treshValue] = 0
--- image_processor.py
+++ image_processor.py
@@ -1,10 +1,11 @@
 from RidgeDetection.ridge_detector import RidgeDetector, DetectionType
-from Thresholding.thresholder import Thresholder, AdaptiveThreshType
+from Thresholding.thresholder import Thresholder, DetectionType
 import glob
 import cv2
 import os
 import statistics
 import numpy
+from PIL import Image
 numpy.set_printoptions(threshold=numpy.nan)
 
 class ImageProcessor:
@@ -14,15 +15,16 @@ class ImageProcessor:
         self.thresholder = Thresholder()
         self.file_list = None
         self.training_data = []
+        self.training_data_mask = []
 
         self.output_data = {"ridge-opencv": [],
                             "ridge-custom": [],
-                            "thresh-gaussian": [],
+                            "thresh-custom": [],
                             "thresh-mean": []}
 
         self.out_dirs = {"ridge-opencv": "DRIVE\\processed\\ridge-detection-opencv\\",
                          "ridge-custom": "DRIVE\\processed\\ridge-detection-custom\\",
-                         "thresh-gaussian": "DRIVE\\processed\\thresholding-gaussian\\",
+                         "thresh-custom": "DRIVE\\processed\\thresholding-custom\\",
                          "thresh-mean": "DRIVE\\processed\\thresholding-mean\\"}
 
         self.create_output_dirs()
@@ -33,9 +35,18 @@ class ImageProcessor:
                 os.makedirs(directory)
 
     def load_data(self):
+        self.load_data_images()
+        self.load_data_masks()
+
+    def load_data_images(self):
         self.file_list = glob.glob('DRIVE\\training\\images\\*.tif')
         for filename in self.file_list:
             self.training_data.append((filename.rsplit('\\', 1)[-1].rsplit('.')[0], cv2.imread(filename)))
+        
+    def load_data_masks(self):
+        self.file_list = glob.glob('DRIVE\\training\\mask\\*.gif')
+        for filename in self.file_list:
+            self.training_data_mask.append(numpy.asarray(Image.open(filename)))
 
     def save_data(self):
         for dataset, images in self.output_data.items():
@@ -59,11 +70,19 @@ class ImageProcessor:
             out_ridge_custom[out_ridge_custom <= ridge_thresholding*statistics.median(out_ridge_custom.ravel())] = 0
             self.output_data["ridge-custom"].append((image[0], out_ridge_custom))
 
+        self.proces_data_threasholding()
+    
+    def proces_data_threasholding(self):
+        for image in self.training_data:
+            image = (image[0], cv2.cvtColor(image[1], cv2.COLOR_BGR2GRAY))
+            out_thresh_mean = self.thresholder.thresh(image[1], DetectionType.OPENCV)
+            self.output_data["thresh-mean"].append((image[0], out_thresh_mean)) 
 
-            out_thresh_gaussian = self.thresholder.thresh(image[1], AdaptiveThreshType.GAUSSIAN)
-            out_thresh_mean = self.thresholder.thresh(image[1], AdaptiveThreshType.MEAN)
-            self.output_data["thresh-gaussian"].append((image[0], out_thresh_gaussian))
-            self.output_data["thresh-mean"].append((image[0], out_thresh_mean))
+        for image in self.training_data:
+            mask = self.training_data_mask[self.training_data.index(image)]
+            out_thresh_custom = self.thresholder.thresh(image[1], DetectionType.CUSTOM, mask=mask)
+            self.output_data["thresh-custom"].append((image[0], out_thresh_custom))
+    
 
     def get_processed_data(self):
         return self.output_data
